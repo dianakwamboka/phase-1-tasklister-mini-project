4545c0f835fa2ef5a1d1ab644e2b159380c14a69
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-28 12:55:58 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.querySelector("#id-list").addEventListener("click", (event) => 
+  {
+    document.getElementById("create-task-form").innerHTML += "Sorry! preventDefault!";
+    event.preventDefault();
+  }, false); 
+
